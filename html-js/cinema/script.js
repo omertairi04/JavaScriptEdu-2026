04776f0ecd0f4e60c73a53c3454c0cc879f424e7
Omer Tairi <tairiomer04@gmail.com>
2026-07-13 19:21:03 +0200

    Cinema Ticket

---
+++ html-js/cinema/script.js
@@ -0,0 +1,31 @@
+const seats = document.getElementById("seats");
+const bln = document.getElementById("balance");
+let balance = 100;
+
+bln.innerText = balance;
+let cmimi = 5;
+for (let i = 0; i < 15; i++) {
+  const btn = document.createElement("button");
+  btn.innerText = "$" + cmimi;
+  seats.append(btn);
+  cmimi += 5;
+  const lv = btn.innerText.split("$")[1];
+
+  btn.addEventListener("click", () => {
+    if (balance < lv) {
+      alert("Nuk ki pare!");
+      return;
+    }
+    
+
+    balance -= lv;
+    bln.innerText = balance;
+
+    btn.style.backgroundColor = "red";
+  });
+}
+
+function shtoPare() {
+  balance += 5;
+  bln.innerText = balance;
+}
